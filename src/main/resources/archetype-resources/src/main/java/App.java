682518b1ce329fd61840c5803fb5865ccc946d68
Nickolas <nk.voloshchuk@gmail.com>
2023-02-05 00:37:11 +0200

    create hello project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.evilcorp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
